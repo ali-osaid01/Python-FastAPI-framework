734079f6d42c9d6189ea8cb96d2784f913ce7b86
ali-osaid01 <aliusaid55@gmail.com>
2024-10-03 05:03:54 +0500

    refactor:add authentication & user Model

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from utils.helpers import generateResponse
+from controllers.todoController import router as todoRouter
+from controllers.rootController import router as rootRouter
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.detail, "statusCode": exc.status_code},
+    )
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello, FastAPI is running!"}
+def read_root():
+    return generateResponse("Welcome to the Todo API")
+
+
+app.include_router(rootRouter)
+app.include_router(todoRouter)
